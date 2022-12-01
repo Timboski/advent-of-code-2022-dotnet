40899e884de5ecb3fdf71a0fc231dd57be019365
timboski <timboboyle@yahoo.co.uk>
2022-12-01 21:14:40 +0000

    Solve day1 part 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("day1-input.txt");
+
+var total = 0;
+var max = 0;
+foreach (var line in lines)
+{
+    try
+    {
+        var calories = int.Parse(line);
+        total += calories;
+    }
+    catch 
+    {
+        if (total > max) max = total;
+        total = 0;
+    }
+}
+if (total> max) max = total;
+
+Console.WriteLine($"Max = {max}");
