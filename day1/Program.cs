0ef991579df94b424b5515569f0ff6966abccfb8
timboski <timboboyle@yahoo.co.uk>
2022-12-01 22:05:38 +0000

    Extract list splitting functionality

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,32 +1,33 @@
-﻿var lines = File.ReadAllLines("day1-input.txt");
+﻿var input = File.ReadAllLines("day1-input.txt");
 
-var elves = new List<int>();
-var total = 0;
-foreach (var line in lines)
-{
-    try
-    {
-        var calories = int.Parse(line);
-        total += calories;
-    }
-    catch 
-    {
-        elves.Add(total);
-        total = 0;
-    }
-}
-
-elves.Add(total);
+var elves = Split(input).ToList();
 elves.Sort();
 elves.Reverse();
-var max = elves.First();
 
+var max = elves.First();
 Console.WriteLine($"Max = {max}");
 
 var topThree = elves.Take(3).Sum();
-
 Console.WriteLine($"Top Three = {topThree}");
 
 // Regression
 if (max != 67027 || topThree != 197291)
     Console.WriteLine("ERROR: INCORRECT RESULT");
+
+static IEnumerable<int> Split(IEnumerable<string> input)
+{
+    var total = 0;
+    foreach (var item in input) 
+    {
+        if (int.TryParse(item, out int calories))
+        {
+            total += calories;
+        }
+        else
+        {
+            yield return total;
+            total = 0;
+        }
+    }
+    yield return total;
+}
