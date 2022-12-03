8504b5fbf0a65447e29cd98ecdb8fb7b139b601b
timboski <timboboyle@yahoo.co.uk>
2022-12-03 21:50:13 +0000

    Simplify strategy scoring

--- day2/GameStrategy.cs
+++ day2/GameStrategy.cs
@@ -3,8 +3,5 @@
 public class GameStrategy
 {
     public static int Solve(IEnumerable<string> strategy)
-        => strategy.Select(ComputeScore).Sum();
-
-    private static int ComputeScore(string strategyEntry)
-        => GameRound.Score(strategyEntry);
+        => strategy.Select(GameRound.Score).Sum();
 }
