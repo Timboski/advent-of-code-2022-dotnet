a3bf5bce45f2a77f37c70a3eae2c0c32400cba4d
timboski <timboboyle@yahoo.co.uk>
2022-12-03 23:07:14 +0000

    Refactor to remove temporary variables

--- Day3/ScoreRucksack.cs
+++ Day3/ScoreRucksack.cs
@@ -4,11 +4,7 @@ public static class ScoreRucksack
 {
     public static char Examine(string contents)
     {
-        var len = contents.Length;
-        var half = len / 2;
-        var start = contents[0..half];
-        var end = contents[half..^0];
-        var duplicates = start.Intersect(end);
-        return duplicates.First();
+        var half = contents.Length / 2;
+        return contents[0..half].Intersect(contents[half..^0]).First();
     }
 }
