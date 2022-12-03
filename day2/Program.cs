03fe6c2b2df4c91bb15a722fabc231d94c1997c0
timboski <timboboyle@yahoo.co.uk>
2022-12-03 22:31:45 +0000

    Run part 2 on real data

--- day2/Program.cs
+++ day2/Program.cs
@@ -2,4 +2,7 @@
 
 var input = File.ReadAllLines("day2-input.txt");
 var result = GameStrategy.Solve(input);
-Console.WriteLine(result);
+Console.WriteLine($"Part 1: {result}");
+
+var result2 = GameStrategy.SolveFixed(input);
+Console.WriteLine($"Part 2: {result2}");
