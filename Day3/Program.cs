72d7ec909e9bcc44468a35a0cd1856a649f98c77
timboski <timboboyle@yahoo.co.uk>
2022-12-04 00:13:45 +0000

    Find part 2 answer

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,4 +2,7 @@
 
 var input = File.ReadAllLines("day3-input.txt");
 var result = input.Select(line => ScoreRucksack.FindPriority(ScoreRucksack.Examine(line))).Sum();
-Console.WriteLine(result);
+Console.WriteLine($"Part1: {result}");
+
+var result2 = ElfGroup.BadgePriority(input);
+Console.WriteLine($"Part2: {result2}");
