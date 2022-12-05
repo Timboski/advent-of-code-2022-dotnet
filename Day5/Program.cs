ff8e1d17e6d3231d60c2351a75068b85240ee119
timboski <timboboyle@yahoo.co.uk>
2022-12-05 23:46:56 +0000

    Solve day 5 part 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -3,4 +3,8 @@
 var lines = File.ReadLines("day5-input.txt");
 var sut = new Ship();
 var result = sut.Process(lines);
-Console.WriteLine(result);
+Console.WriteLine($"Part 1: {result}");
+
+var sut2 = new Ship();
+var result2 = sut.WhenProcessCraneMover9001(lines);
+Console.WriteLine($"Part 2: {result2}");
