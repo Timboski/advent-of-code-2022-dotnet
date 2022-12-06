ff300c598e72eb872c91eff9357a53296887520b
timboski <timboboyle@yahoo.co.uk>
2022-12-06 21:18:52 +0000

    FInd solution to day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,3 +3,6 @@
 var data = File.ReadAllText("day6-input.txt");
 var packetStart = DataStreamDecoder.FindStartOfPacket(data);
 Console.WriteLine($"Part 1: {packetStart}");
+
+var messageStart = DataStreamDecoder.FindStartOfMessage(data);
+Console.WriteLine($"Part 2: {messageStart}");
