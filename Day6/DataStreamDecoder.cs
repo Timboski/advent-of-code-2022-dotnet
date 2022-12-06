194cecc7e9db935fec37584a445c2b1cd3fa3080
timboski <timboboyle@yahoo.co.uk>
2022-12-06 21:10:35 +0000

    Refactor to allow changing block length

--- Day6/DataStreamDecoder.cs
+++ Day6/DataStreamDecoder.cs
@@ -2,16 +2,24 @@
 
 public static class DataStreamDecoder
 {
-    public static int FindStartOfPacket(string data)
+    public static int FindStartOfPacket(string data) 
+        => FindNonRepeatingBlock(data, 4);
+
+    public static object FindStartOfMessage(string data)
     {
-        for (var index = 4;  index <= data.Length; ++index)
+        throw new NotImplementedException();
+    }
+
+    public static int FindNonRepeatingBlock(string data, int blockLength)
+    {
+        for (var index = blockLength;  index <= data.Length; ++index)
         {
-            if (IsValidPacketStart(data[(index - 4)..index])) return index;
+            if (IsNonRepeatingBlock(data[(index - blockLength)..index])) return index;
         }
         throw new IndexOutOfRangeException("Packet start not found");
     }
 
-    private static bool IsValidPacketStart(string packetStart)
+    private static bool IsNonRepeatingBlock(string packetStart)
     {
         if (packetStart[0] == packetStart[1]) return false;
         if (packetStart[0] == packetStart[2]) return false;
