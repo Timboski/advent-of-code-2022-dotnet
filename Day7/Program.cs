085fdcf03a3703e79f1d93ffb0540dab17cb8100
timboski <timboboyle@yahoo.co.uk>
2022-12-07 23:56:31 +0000

    Solve day 7 part 1

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day7;
+
+var tree = new SystemDirectory();
+var sut = new TerminalOutputDecoder(tree);
+var lines = File.ReadAllLines("day7-input.txt");
+foreach (var line in lines) sut.ProcessLine(line);
+var result = tree.ListDirectories().Select(d => d.Size).Where(s => s <= 100000).Sum();
+Console.WriteLine(result);
