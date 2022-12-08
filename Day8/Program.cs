8555b83d5b4fb583ccabcd28a148942122a72526
timboski <timboboyle@yahoo.co.uk>
2022-12-08 22:37:01 +0000

    Solve day 8 part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Day8;
 
 var rows = File.ReadAllLines("day8-input.txt");
-var numberOfTreesVisible = new TreePatch(rows).CountVisible();
+var sut = new TreePatch(rows);
 
+var numberOfTreesVisible = sut.CountVisible();
 Console.WriteLine($"Part 1: {numberOfTreesVisible}");
+
+var scenicScore = sut.GetMaxScenicScore();
+Console.WriteLine($"Part 2: {scenicScore}");
