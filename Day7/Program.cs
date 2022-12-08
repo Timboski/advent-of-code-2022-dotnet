1982cc0256f2765697725bab0deb6c3a98f0093b
timboski <timboboyle@yahoo.co.uk>
2022-12-08 00:09:06 +0000

    Solve day 7 part 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -5,4 +5,10 @@ var sut = new TerminalOutputDecoder(tree);
 var lines = File.ReadAllLines("day7-input.txt");
 foreach (var line in lines) sut.ProcessLine(line);
 var result = tree.ListDirectories().Select(d => d.Size).Where(s => s <= 100000).Sum();
-Console.WriteLine(result);
+Console.WriteLine($"Part 1: {result}");
+
+var max = 70000000 - 30000000;
+var target = tree.Size - max;
+var largeEnoughDirectories = tree.ListDirectories().Select(d => d.Size).Where(s => s >= target);
+var bestMatch = largeEnoughDirectories.OrderDescending().Reverse().First();
+Console.WriteLine($"Part 2: {bestMatch}");
