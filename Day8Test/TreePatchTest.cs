53dec770cafae9854cbf8f0e77cb021a5f9ce36f
timboski <timboboyle@yahoo.co.uk>
2022-12-08 21:56:43 +0000

    Add regression test for day 8 part 1

--- Day8Test/TreePatchTest.cs
+++ Day8Test/TreePatchTest.cs
@@ -4,6 +4,7 @@ public class TreePatchTest
 {
     [Theory]
     [InlineData("day8-example-input.txt", 21)]
+    [InlineData("day8-input.txt", 1807)]
     public void GivenExampleMap_WhenCheckVisibility_ReturnsExpectedCount(string filename, int expectedCount)
     {
         // Arrange
