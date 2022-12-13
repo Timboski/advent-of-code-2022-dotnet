f69ee828d621be7f43239fa1daff9b5e94e77578
timboski <timboboyle@yahoo.co.uk>
2022-12-13 23:08:51 +0000

    Solve day 10 part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -2,3 +2,7 @@
 
 var signalStrength = new CPU().FindSignalStrength("day10-input.txt");
 Console.WriteLine($"Part 1: {signalStrength}");
+
+var render = new CPU().RenderScreen("day10-input.txt");
+Console.WriteLine("Part 2:");
+Console.WriteLine(render);
