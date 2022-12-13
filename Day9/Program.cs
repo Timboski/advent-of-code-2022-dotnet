d25ba3b0597e539ca09ef4717879f71766fb2755
timboski <timboboyle@yahoo.co.uk>
2022-12-13 21:11:17 +0000

    Solve day 9 part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -2,4 +2,10 @@
 
 var sut = new RopeTracker();
 sut.ParseFile("day9-input.txt");
-Console.WriteLine(sut.TailVisits);
+Console.WriteLine($"Part 1: {sut.TailVisits}");
+
+var origin = new EndPosition(0, 0);
+var tenKnots = new TenKnots(origin);
+var sut2 = new RopeTracker(tenKnots);
+sut2.ParseFile("day9-input.txt");
+Console.WriteLine($"Part 2: {sut2.TailVisits}");
