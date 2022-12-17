2e54d282eadfad86a7841917800cf920b4a64a17
timboski <timboboyle@yahoo.co.uk>
2022-12-17 11:57:00 +0000

    Impement old*old operation

--- Day11/Monkey.cs
+++ Day11/Monkey.cs
@@ -3,23 +3,49 @@
 public class Monkey
 {
 	private Queue<int> _items = new();
+    private readonly Func<int, int> _worryEvaulation;
 
 	public Monkey(List<string> monkeyData)
-	{
-		// e.g.  "  Starting items: 79, 98"
-		var items = monkeyData[0]
-			.Split("Starting items:")[1]
-			.Split(",")
-			.Select(int.Parse);
-		foreach (var item in items) _items.Enqueue(item);
-		
+    {
+        ParseStartingItems(monkeyData[0]);
+        _worryEvaulation = ParseWorryEvaluation(monkeyData[1]);
+
     }
 
-	public int[] Items => _items.ToArray();
+    public int[] Items => _items.ToArray();
 
     public bool InspectItem(out int targetMonkey, out int worryLevel)
     {
 		targetMonkey = 0;
-		return _items.TryDequeue(out worryLevel);
+        worryLevel = 0;
+		if (!_items.TryDequeue(out int item)) return false;
+
+        worryLevel = _worryEvaulation(item);
+        return true;
+    }
+
+    /// <example>
+    /// String format: "  Starting items: 79, 98"
+    /// </example>
+    private void ParseStartingItems(string itemsList)
+    {
+        var items = itemsList
+            .Split("Starting items:")[1]
+            .Split(",")
+            .Select(int.Parse);
+        foreach (var item in items) _items.Enqueue(item);
+    }
+
+    /// <example>
+    /// "  Operation: new = old * 19"
+    /// "  Operation: new = old + 6"
+    /// "  Operation: new = old * old"
+    /// "  Operation: new = old + 3"
+    /// </example>
+    private static Func<int, int> ParseWorryEvaluation(string operation)
+    {
+        var rule = operation.Split("Operation: new = old ")[1];
+        if (rule == "* old") return a => a * a;
+        throw new NotImplementedException(rule);
     }
 }
