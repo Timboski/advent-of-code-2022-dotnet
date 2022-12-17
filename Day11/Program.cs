d351b051b670c6a9aca7ffa5c72c4538e1809861
timboski <timboboyle@yahoo.co.uk>
2022-12-17 14:22:41 +0000

    Compute answer (get 121103, too high)

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day11;
+
+var sut = new MonkeySimulation("day11-input.txt");
+
+// Act
+var monkeyBusiness = sut.FindMonkeyBusiness(20);
+Console.WriteLine($"Part 1: {monkeyBusiness}");
