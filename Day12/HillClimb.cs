edd57bf4449b4d5da66321df9bbc2349ab1450c1
timboski <timboboyle@yahoo.co.uk>
2022-12-18 23:34:40 +0000

    Attempt to solve part 1 (too slow)

--- Day12/HillClimb.cs
+++ Day12/HillClimb.cs
@@ -12,8 +12,22 @@ public class HillClimb
 
     public int FindPath()
     {
+		var processedMaps = 0;
+		var throttle = 0;
 		while (_heightMap.TryDequeue(out HeightMap heightMap)) 
 		{
+			++processedMaps;
+			++throttle;
+			if (throttle >= 100000)
+			{
+				throttle = 0;
+                Console.WriteLine($"Iteration: {processedMaps}");
+                Console.WriteLine($"Path Length: {heightMap.PathLength}");
+                Console.WriteLine($"Queue Length: {_heightMap.Count}");
+                Console.WriteLine(heightMap.ToString());
+				Console.WriteLine();
+			}
+
 			if (heightMap.IsComplete) return heightMap.PathLength;
 
 			foreach (var step in heightMap.FindSteps()) _heightMap.Enqueue(step);
--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day12;
+
+var sut = new HillClimb("day12-input.txt");
+var steps = sut.FindPath();
+Console.WriteLine($"Part 1: {steps}");
