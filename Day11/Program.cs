262f2df4c7760e55c5c666160813a3375a1275cb
timboski <timboboyle@yahoo.co.uk>
2022-12-18 00:11:01 +0000

    Fix part 2 executable

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -5,5 +5,5 @@ var monkeyBusiness = sut.FindMonkeyBusiness(20);
 Console.WriteLine($"Part 1: {monkeyBusiness}");
 
 var sut2 = new MonkeySimulation("day11-input.txt");
-var monkeyBusiness2 = sut.FindMonkeyBusiness(10000);
+var monkeyBusiness2 = sut2.FindMonkeyBusiness(10000);
 Console.WriteLine($"Part 2: {monkeyBusiness2}");
