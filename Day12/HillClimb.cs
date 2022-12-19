7ca8c4269951cdc3e9306efb7de13b8838741be2
timboski <timboboyle@yahoo.co.uk>
2022-12-19 23:10:17 +0000

    Remove debug output

--- Day12/HillClimb.cs
+++ Day12/HillClimb.cs
@@ -15,10 +15,6 @@ public class HillClimb
 
     public int FindPath()
     {
-        var start = _heightMap.Peek();
-
-		var processedMaps = 0;
-		var throttle = 0;
 		while (_heightMap.TryDequeue(out HeightMap heightMap))
         {
             var pathLength = heightMap.PathLength;
@@ -27,39 +23,13 @@ public class HillClimb
             {
                 _visited[stepStart] = pathLength;
 
-                ++processedMaps;
-                ++throttle;
-                if (throttle >= 100)
-                {
-                    throttle = 0;
-                    DisplayState(processedMaps, heightMap);
-                }
-
-                if (heightMap.IsComplete)
-                {
-                    Console.WriteLine("COMPLETED");
-                    DisplayState(processedMaps, heightMap);
-                    return heightMap.PathLength;
-                }
+                if (heightMap.IsComplete) return heightMap.PathLength;
 
                 foreach (var step in heightMap.FindSteps())
                     _heightMap.Enqueue(step);
             }
 		}
 
-		Console.WriteLine("FAILED");
-		Console.WriteLine(processedMaps);
-        Console.WriteLine($"Queue Length: {_heightMap.Count}");
-        Console.WriteLine(start.GetVisitedMap(_visited));
         throw new PathNotFoundException();
-
-        void DisplayState(int processedMaps, HeightMap heightMap)
-        {
-            Console.WriteLine(heightMap.ToString());
-            Console.WriteLine($"Iteration: {processedMaps}");
-            Console.WriteLine($"Path Length: {heightMap.PathLength}");
-            Console.WriteLine($"Queue Length: {_heightMap.Count}");
-            Console.WriteLine();
-        }
     }
 }
