2be60134fb504b5803b9fa33f4836e7400a9fcbe
timboski <timboboyle@yahoo.co.uk>
2022-12-19 23:43:59 +0000

    Fix test name

--- Day12Test/HillCLimbTest.cs
+++ Day12Test/HillCLimbTest.cs
@@ -5,7 +5,7 @@ public class HillCLimbTest
     [Theory]
     [InlineData("day12-example-input.txt", 31)]
     [InlineData("day12-input.txt", 472)]
-    public void GivenMapDataWithNoStart_WhenCreateHeightMap_ThrowsException(string filename, int expectedNumberOfSteps)
+    public void GivenHillCLimbWithMapData_WhenFindPath_ReturnsNumberOfSteps(string filename, int expectedNumberOfSteps)
     {
         // Arrange
         var sut = new HillClimb(filename);
