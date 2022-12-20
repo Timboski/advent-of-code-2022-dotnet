1f74f9d997e59156896d78da63c5a1f1020b900e
timboski <timboboyle@yahoo.co.uk>
2022-12-20 21:13:03 +0000

    Check uneven lengths when differs before

--- Day13Test/ArrayElementTest.cs
+++ Day13Test/ArrayElementTest.cs
@@ -6,6 +6,8 @@ public class ArrayElementTest
     [InlineData("[1,1,3,1,1]", "[1,1,5,1,1]", Order.Correct)]
     [InlineData("[2,1,3,1,1]", "[1,1,5,1,1]", Order.Wrong)]
     [InlineData("[1,1,3,1,2]", "[1,1,5,1,1]", Order.Correct)]
+    [InlineData("[1,2,3]", "[1,1,5,1,1]", Order.Wrong)]
+    [InlineData("[1,1,3,4,5]", "[1,2,5]", Order.Correct)]
     public void GivenTwoArrayStrings_WhenParseAndCompare_ReturnsExpectedOrder(string leftArray, string rightArray, Order expectedOrder)
     {
         // Arrange
