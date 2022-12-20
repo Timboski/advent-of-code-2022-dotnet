903f8aacddab8231ec5a0fd1b41ec40c8ef6a121
timboski <timboboyle@yahoo.co.uk>
2022-12-20 21:12:41 +0000

    Fix test

--- Day13Test/ArrayElementTest.cs
+++ Day13Test/ArrayElementTest.cs
@@ -5,7 +5,7 @@ public class ArrayElementTest
     [Theory]
     [InlineData("[1,1,3,1,1]", "[1,1,5,1,1]", Order.Correct)]
     [InlineData("[2,1,3,1,1]", "[1,1,5,1,1]", Order.Wrong)]
-    [InlineData("[1,1,3,1,2]", "[1,1,5,1,1]", Order.Wrong)]
+    [InlineData("[1,1,3,1,2]", "[1,1,5,1,1]", Order.Correct)]
     public void GivenTwoArrayStrings_WhenParseAndCompare_ReturnsExpectedOrder(string leftArray, string rightArray, Order expectedOrder)
     {
         // Arrange
