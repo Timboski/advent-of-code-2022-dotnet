d17ec4ba5571f6f0d7368869b5a82cb7c1f16eb9
timboski <timboboyle@yahoo.co.uk>
2022-12-20 21:21:24 +0000

    Check when left array is shorter

--- Day13/ArrayElement.cs
+++ Day13/ArrayElement.cs
@@ -1,4 +1,6 @@
-﻿namespace Day13;
+﻿using System.Linq;
+
+namespace Day13;
 
 public class ArrayElement
 {
@@ -14,13 +16,15 @@ public class ArrayElement
 
     public Order CheckOrder(ArrayElement other)
     {
-        for (int i = 0; i < _elements.Count; ++i) 
+        var smallestArraySize = Math.Min(_elements.Count, other._elements.Count);
+        for (int i = 0; i < smallestArraySize; ++i) 
         {
             Order order = _elements[i].CheckOrder(other._elements[i]);
             if (order != Order.Equal) return order;
         }
 
         if (other._elements.Count > _elements.Count) return Order.Correct;
+        if (other._elements.Count < _elements.Count) return Order.Wrong;
 
         return Order.Equal;
     }
