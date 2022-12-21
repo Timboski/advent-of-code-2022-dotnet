96aca28a90834ae1cb63ccb176830fc541322f76
timboski <timboboyle@yahoo.co.uk>
2022-12-21 21:17:06 +0000

    Solve day 13 part 2

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -2,3 +2,7 @@
 
 var score = CheckPairs.FromFile("day13-input.txt");
 Console.WriteLine($"Part 1: {score}");
+
+// Act
+var key = DecoderKey.Find("day13-input.txt");
+Console.WriteLine($"Part 2: {key}");
