66a797f0cc97eef11666f4655a4abb4dc71c4605
timboski <timboboyle@yahoo.co.uk>
2022-12-21 00:11:58 +0000

    Regression test for day 13 part 1

--- Day13Test/CheckPairsTest.cs
+++ Day13Test/CheckPairsTest.cs
@@ -4,6 +4,7 @@ public class CheckPairsTest
 {
     [Theory]
     [InlineData("day13-example-input.txt", 13)]
+    [InlineData("day13-input.txt", 6428)]
     public void GivenDistressSignal_WhenFindCorrectPairs_ReturnsCorrectSumOfIndecies(string filename, int expectedScore)
     {
         // Arrange
