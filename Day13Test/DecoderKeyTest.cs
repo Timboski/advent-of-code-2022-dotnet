7e7d8788316fa838aad562d8f33d34377d3caa2f
timboski <timboboyle@yahoo.co.uk>
2022-12-21 21:17:36 +0000

    Regression test for day 13 part 2

--- Day13Test/DecoderKeyTest.cs
+++ Day13Test/DecoderKeyTest.cs
@@ -4,7 +4,7 @@ public class DecoderKeyTest
 {
     [Theory]
     [InlineData("day13-example-input.txt", 140)]
-    // [InlineData("day13-input.txt", ???)]
+    [InlineData("day13-input.txt", 22464)]
     public void GivenDistressSignal_WhenFindDecoderKey_ReturnsCorrectValue(string filename, int expectedKey)
     {
         // Arrange
