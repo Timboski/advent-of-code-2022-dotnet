f95c1a215d7351f4c588f89e0e025ac98b4d0456
timboski <timboboyle@yahoo.co.uk>
2022-12-30 16:25:32 +0000

    Check second example line

--- Day14Test/FileParserTest.cs
+++ Day14Test/FileParserTest.cs
@@ -5,7 +5,8 @@ namespace Day14Test;
 public class FileParserTest
 {
     [Theory]
-    [InlineData("498,4 -> 498,6 -> 496,6", new [] { 498, 498, 496 }, new [] { 4, 6, 6 })]
+    [InlineData("498,4 -> 498,6 -> 496,6", new[] { 498, 498, 496 }, new[] { 4, 6, 6 })]
+    [InlineData("503,4 -> 502,4 -> 502,9 -> 494,9", new[] { 503, 502, 502, 494 }, new[] { 4, 4, 9, 9 })]
     public void GivenStringRepresentation_WhenReadPath_ReturnsListOfPoints(string pathData, int[] xCoords, int[] yCoords)
     {
         // Arrange
