261ed738435fbc8a9177cc255fb94a2f5e3e1cce
timboski <timboboyle@yahoo.co.uk>
2022-12-30 19:21:02 +0000

    Plot the starting map

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day14;
+using System.Drawing;
+
+var filename = "day14-input.txt";
+var box = FileParser.FindBoundngBox(filename);
+var paths = FileParser.ReadFile(filename);
+var cave = new Cave(box, new Point(500, 0));
+cave.AddPaths(paths);
+
+Console.WriteLine("Part1 : Initial map");
+Console.WriteLine(cave.ToString());
