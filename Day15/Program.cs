a1f79dd67a58f973166b33ae5657f523df6aba51
timboski <timboboyle@yahoo.co.uk>
2022-12-31 13:03:44 +0000

    FInd day 15 part 1 solution

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day15;
+
+var sut = new Scan("day15-input.txt");
+var positions = sut.FindExcludedPositions(2000000);
+var count = positions.Count();
+Console.WriteLine($"Part 1: {count}");
