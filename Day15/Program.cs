554061e5958d3547d12fca360ea8ca6fc343a001
timboski <timboboyle@yahoo.co.uk>
2022-12-31 13:30:51 +0000

    Find beacon for real data (too slow!)

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -4,3 +4,6 @@ var sut = new Scan("day15-input.txt");
 var positions = sut.FindExcludedPositions(2000000);
 var count = positions.Count();
 Console.WriteLine($"Part 1: {count}");
+
+var tuningFrequency = sut.FindTuningFrequency(4000000);
+Console.WriteLine($"Part 2: {tuningFrequency}");
