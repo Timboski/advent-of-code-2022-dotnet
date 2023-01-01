ea7f5f9b27d247ecbe8f6dd1aaf2f82ded8f5ed9
timboski <timboboyle@yahoo.co.uk>
2023-01-01 23:46:46 +0000

    Solve part 1

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day16;
+
+var sut = new VolcanoPathFinder("day16-input.txt");
+var pressure = sut.MaximisePressure();
+Console.WriteLine($"Part 1 {pressure}");
