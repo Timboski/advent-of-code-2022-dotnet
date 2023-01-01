cd8b7408959e654981c5e26f044a158c8f3910ea
timboski <timboboyle@yahoo.co.uk>
2023-01-01 20:30:42 +0000

    Read pressure

--- Day16/VolcanoMaze.cs
+++ Day16/VolcanoMaze.cs
@@ -2,15 +2,20 @@
 
 public class VolcanoMaze
 {
-	private class Valve
+    /// <example>
+    /// "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
+    /// </example>
+    private class Valve
 	{
 		public Valve(string data)
 		{
 			Name = data[6..8];
+			Pressure = int.Parse(data.Split('=')[1].Split(';')[0]);
 		}
 
 		public string Name { get; }
-	}
+        public int Pressure { get; }
+    }
 
 	private readonly Dictionary<string, Valve> _valves;
 
@@ -25,4 +30,6 @@ public class VolcanoMaze
 	}
 
 	public string Valves => string.Concat(_valves.Keys.Order());
+
+	public int GetPressure(string valve) => _valves[valve].Pressure;
 }
--- Day16Test/VolcanoMazeTest.cs
+++ Day16Test/VolcanoMazeTest.cs
@@ -15,4 +15,30 @@ public class VolcanoMazeTest
         // Assert
         Assert.Equal(expectedValves, sut.Valves);
     }
+
+    [Fact]
+    public void GivenInputFile_WhenCreateVolcanoMaze_ValvesHaveCorrectPressure()
+    {
+        // Arrange
+        var filename = "day16-example-input.txt";
+        var expectedValves = new (string Valve, int Pressure)[] {
+                ("AA", 0),
+                ("BB", 13),
+                ("CC", 2),
+                ("DD", 20),
+                ("EE", 3),
+                ("FF", 0),
+                ("GG", 0),
+                ("HH", 22),
+                ("II", 0),
+                ("JJ", 21),
+            };
+
+        // Act
+        var sut = new VolcanoMaze(filename);
+
+        // Assert
+        foreach (var valve in expectedValves )
+            Assert.Equal(valve.Pressure, sut.GetPressure(valve.Valve));
+    }
 }
