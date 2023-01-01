8570d5787f4051a354e8734a353997487c526b6d
timboski <timboboyle@yahoo.co.uk>
2023-01-01 21:46:22 +0000

    Tidy

--- Day16/VolcanoMaze.cs
+++ Day16/VolcanoMaze.cs
@@ -2,38 +2,38 @@
 
 public class VolcanoMaze
 {
-	/// <example>
-	/// "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
-	/// </example>
-	private class Valve
-	{
-		public Valve(string data)
-		{
-			Name = data[6..8];
-			Pressure = int.Parse(data.Split('=')[1].Split(';')[0]);
-			Paths = data.Split(',').Select(a => a[^2..^0]).ToArray();
-		}
+    /// <example>
+    /// "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
+    /// </example>
+    private class Valve
+    {
+        public Valve(string data)
+        {
+            Name = data[6..8];
+            Pressure = int.Parse(data.Split('=')[1].Split(';')[0]);
+            Paths = data.Split(',').Select(a => a[^2..^0]).ToArray();
+        }
 
-		public string Name { get; }
-		public int Pressure { get; }
-		public string[] Paths { get; }
-	}
+        public string Name { get; }
+        public int Pressure { get; }
+        public string[] Paths { get; }
+    }
 
-	private readonly Dictionary<string, Valve> _valves;
+    private readonly Dictionary<string, Valve> _valves;
 
-	public VolcanoMaze(string filename)
-	{
-		static Valve CreateValve(string data) => new Valve(data);
+    public VolcanoMaze(string filename)
+    {
+        static Valve CreateValve(string data) => new(data);
 
-		_valves = File
-			.ReadAllLines(filename)
-			.Select(CreateValve)
-			.ToDictionary(v => v.Name, v => v);
-	}
+        _valves = File
+            .ReadAllLines(filename)
+            .Select(CreateValve)
+            .ToDictionary(v => v.Name, v => v);
+    }
 
-	public string Valves => string.Concat(_valves.Keys.Order());
+    public string Valves => string.Concat(_valves.Keys.Order());
 
-	public int GetPressure(string valve) => _valves[valve].Pressure;
+    public int GetPressure(string valve) => _valves[valve].Pressure;
 
-	public IEnumerable<string> GetPaths(string valve) => _valves[valve].Paths;
+    public IEnumerable<string> GetPaths(string valve) => _valves[valve].Paths;
 }
