b7a6fd5347b70428e02c7501b78dd9ce8d6f1cae
timboski <timboboyle@yahoo.co.uk>
2023-01-01 23:47:33 +0000

    Regression for day 16 part 1

--- Day16Test/VolcanoPathFinderTest.cs
+++ Day16Test/VolcanoPathFinderTest.cs
@@ -4,6 +4,7 @@ public class VolcanoPathFinderTest
 {
     [Theory]
     [InlineData("day16-example-input.txt", 1651)]
+    [InlineData("day16-input.txt", 1559)]
     public void GivenVolcanoPathFinder_WhenMaximisePressure_ReturnsMostPressureReleased(string filename, int expectedResult)
     {
         // Arrange
