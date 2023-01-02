8d17992ccf868897a9089c9806f18518546e1613
timboski <timboboyle@yahoo.co.uk>
2023-01-02 13:59:31 +0000

    Account for shorter time

--- Day16/VolcanoPathFinder.cs
+++ Day16/VolcanoPathFinder.cs
@@ -5,22 +5,25 @@ public class VolcanoPathFinder
     private readonly VolcanoMaze _volcano;
     private Queue<(string State, int Pressure)> _states = new();
     private Queue<(string State, int Pressure)> _nextStates = new();
+    private readonly int _initialTimeRemaining;
 
     public VolcanoPathFinder(string filename)
     {
         _volcano = new VolcanoMaze(filename);
         _states.Enqueue(("AA", 0));
+        _initialTimeRemaining = 29;
     }
 
     public VolcanoPathFinder(string filename, Elephant elephant)
     {
         _volcano = new VolcanoMaze(filename, elephant);
         _states.Enqueue(("AAAA", 0));
+        _initialTimeRemaining = 25;
     }
 
     public int MaximisePressure()
     {
-        for (var timeRemaining = 29; timeRemaining >= 1; timeRemaining--) 
+        for (var timeRemaining = _initialTimeRemaining; timeRemaining >= 1; timeRemaining--) 
         {
             ProcessQueue(timeRemaining);
 
