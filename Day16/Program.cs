4af1b00ff4e3a5dd8e9355f2bd043065e4574688
timboski <timboboyle@yahoo.co.uk>
2023-01-03 07:42:17 +0000

    Solve part 2 (ran overnight though)

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -5,5 +5,5 @@ var pressure = sut.MaximisePressure();
 Console.WriteLine($"Part 1 {pressure}");
 
 var sut2 = new VolcanoPathFinder("day16-input.txt", Elephant.WithElephant);
-var pressure2 = sut.MaximisePressure();
+var pressure2 = sut2.MaximisePressure();
 Console.WriteLine($"Part 2 {pressure2}");
