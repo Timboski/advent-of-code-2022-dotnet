1f58948758e2474e34d0e73479dc59372c581d3f
timboski <timboboyle@yahoo.co.uk>
2023-01-07 17:58:44 +0000

    Add regression test for day 18 part 1

--- Day18Test/SurfaceAreaFinderTest.cs
+++ Day18Test/SurfaceAreaFinderTest.cs
@@ -4,6 +4,7 @@ public class SurfaceAreaFinderTest
 {
     [Theory]
     [InlineData("day18-example-input.txt", 64)]
+    [InlineData("day18-input.txt", 3448)]
     public void GivenInputFile_WhenFindArea_FindsExpectedArea(string filename, int expectedArea)
     {
         // Arrange, Act
