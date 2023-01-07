629acc78380b439237390e03f872f73d800f5c3d
timboski <timboboyle@yahoo.co.uk>
2023-01-07 17:31:17 +0000

    Sort boulders into bins with same X

---
+++ Day18/BoulderBins.cs
@@ -0,0 +1,17 @@
+﻿namespace Day18;
+
+public static class BoulderBins
+{
+    public static IEnumerable<IEnumerable<Boulder>> Sort(IEnumerable<Boulder> boulders, Axis axis)
+    {
+        var bins = new Dictionary<int, List<Boulder>>();
+        foreach (var boulder in boulders)
+        {
+            var key = boulder.GetValue(axis);
+            if (!bins.ContainsKey(key)) bins[key] = new List<Boulder>();
+            bins[key].Add(boulder);
+        }
+
+        return bins.Values;
+    }
+}
---
+++ Day18Test/BoulderBinsTest.cs
@@ -0,0 +1,28 @@
+namespace Day18Test;
+
+public class BoulderBinsTest
+{
+    [Fact]
+    public void GivenExampleInputBoulders_WhenSortByXAxis_BouldersWithSameXAreInSameBin()
+    {
+        // Arrange
+        var boulders = File.ReadAllLines("day18-example-input.txt")
+                            .Select(l => new Boulder(l));
+
+        // Act
+        var bins = BoulderBins.Sort(boulders, Axis.X);
+
+        // Assert
+        Assert.Equal(3, bins.Count());
+        var binnedBoulders = 0;
+        foreach (var bin in bins)
+        {
+            binnedBoulders += bin.Count();
+            var expectedX = bin.First().GetValue(Axis.X);
+            foreach (var boulder in bin)
+                Assert.Equal(expectedX, boulder.GetValue(Axis.X));
+        }
+
+        Assert.Equal(boulders.Count(), binnedBoulders);
+    }
+}
