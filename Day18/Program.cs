53c22ca476700db3bf1bfdc17e97b4af5b5dca42
timboski <timboboyle@yahoo.co.uk>
2023-01-08 21:54:02 +0000

    Try to solve (expect low) - 2044 is too low.

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -2,3 +2,6 @@
 
 var area = SurfaceAreaFinder.FindArea("day18-input.txt");
 Console.WriteLine($"Part 1: {area}");
+
+var externalArea = SurfaceAreaFinder.FindExternalArea("day18-input.txt");
+Console.WriteLine($"Part 2: {externalArea}");
